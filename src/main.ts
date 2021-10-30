52aa1b72036a090721191fedae129b7490285f33
Barish Barla <barish.barla10@gmail.com>
2021-10-30 23:44:41 +0530

    Preloading Resources

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,4 @@
-import { Application, Sprite } from 'pixi.js'
+import { Application, Sprite, Loader } from 'pixi.js'
 
 const app = new Application() as Application
 const keys = {}
@@ -19,31 +19,51 @@ app.stage.addChild(shipSprite)
 
 app.stage.interactive = true
 
-window.onkeydown = (e) => {
-  keys[e.keyCode] = true
+const showComplete = () => {
+  console.log("Done Loading")
+
+  const ship_01 = Sprite.from(app.loader.resources.ship01.texture)
+  ship_01.anchor.set(0.5)
+  ship_01.x = app.view.width / 4
+  ship_01.y = app.view.height / 4
+  app.stage.addChild(ship_01)
+  const ship_02 = Sprite.from(app.loader.resources.ship02.texture)
+  ship_02.anchor.set(0.5)
+  ship_02.x = app.view.width * (3 / 4)
+  ship_02.y = app.view.height / 4
+  app.stage.addChild(ship_02)
+  const ship_03 = Sprite.from(app.loader.resources.ship03.texture)
+  ship_03.anchor.set(0.5)
+  ship_03.x = app.view.width / 4
+  ship_03.y = app.view.height * (3 / 4)
+  app.stage.addChild(ship_03)
+  const ship_04 = Sprite.from(app.loader.resources.ship04.texture)
+  ship_04.anchor.set(0.5)
+  ship_04.x = app.view.width * (3 / 4)
+  ship_04.y = app.view.height * (3 / 4)
+  app.stage.addChild(ship_04)
 }
 
-window.onkeyup = (e) => {
-  keys[e.keyCode] = false
+const showProgress = (e: Loader) => {
+  console.log(e.progress)
 }
 
-const gameLoop = () => {
-  if (keys["87"]) {
-    // W key
-    shipSprite.y -= speed
-  }
-  if (keys["83"]) {
-    // S key
-    shipSprite.y += speed
-  }
-  if (keys["65"]) {
-    // A key
-    shipSprite.x -= speed
-  }
-  if (keys["68"]) {
-    // D key
-    shipSprite.x += speed
-  }
+const showError = (e: Error) => {
+  console.error(e.message)
 }
 
-app.ticker.add(gameLoop)
+app.loader.baseUrl = "kenny_pixelshmup/Ships"
+
+app.loader
+  .add("ship01", "ship_0001.png")
+  .add("ship02", "ship_0002.png")
+  .add("ship03", "ship_0003.png")
+  .add("ship04", "ship_0004.png")
+  .add("ship05", "ship_0005.png")
+  .add("ship06", "ship_0006.png")
+  .add("ship07", "ship_0007.png")
+
+app.loader.onProgress.add(showProgress)
+app.loader.onComplete.add(showComplete)
+app.loader.onError.add(showError)
+app.loader.load()
